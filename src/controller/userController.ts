a9caf1bf7c1f6a06baac4c6636051997d366794b
afiqnexea <afiqnrzm@hotmail.com>
2024-04-16 02:05:36 +0800

    a

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -1,9 +1,6 @@
 import { Request, Response } from 'express';
-import { PrismaClient } from '@prisma/client';
 
 import { updateUser } from 'src/service/userServices';
-
-const prisma = new PrismaClient();
 
 export const updateProfile = async (req: Request, res: Response) => {
   //   const { name, email, password, photoURL, designation, country, phoneNumber } = req.body;
@@ -15,46 +12,46 @@ export const updateProfile = async (req: Request, res: Response) => {
   }
 };
 
-export const approveOrReject = async (req: Request, res: Response) => {
-  const { approve } = req.body;
+// export const approveOrReject = async (req: Request, res: Response) => {
+//   const { approve } = req.body;
 
-  try {
-    if (approve) {
-      await prisma.notification.create({
-        data: {
-          receiver_id: 1,
-          content: 'Your pitch has been approved',
-        },
-      });
-      return res.send('You pitch has been approved');
-    }
-    await prisma.notification.create({
-      data: {
-        receiver_id: 1,
-        content: 'Your pitch has been rejected',
-      },
-    });
-    return res.send('You pitch has been rejected');
-  } catch (error) {
-    res.end(error);
-  }
-};
+//   try {
+//     if (approve) {
+//       await prisma.notification.create({
+//         data: {
+//           receiver_id: 1,
+//           content: 'Your pitch has been approved',
+//         },
+//       });
+//       return res.send('You pitch has been approved');
+//     }
+//     await prisma.notification.create({
+//       data: {
+//         receiver_id: 1,
+//         content: 'Your pitch has been rejected',
+//       },
+//     });
+//     return res.send('You pitch has been rejected');
+//   } catch (error) {
+//     res.end(error);
+//   }
+// };
 
-export const getAllNotification = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  try {
-    const data = await prisma.notification.findMany({
-      where: {
-        receiver_id: parseInt(id),
-      },
-    });
+// export const getAllNotification = async (req: Request, res: Response) => {
+//   const { id } = req.params;
+//   try {
+//     const data = await prisma.notification.findMany({
+//       where: {
+//         receiver_id: parseInt(id),
+//       },
+//     });
 
-    if (data.length < 1) {
-      return res.send('No notifcation');
-    }
+//     if (data.length < 1) {
+//       return res.send('No notifcation');
+//     }
 
-    return res.send(data);
-  } catch (error) {
-    return res.send(error);
-  }
-};
+//     return res.send(data);
+//   } catch (error) {
+//     return res.send(error);
+//   }
+// };
--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import { approveOrReject, getAllNotification, updateProfile } from 'src/controller/userController';
+import { updateProfile } from 'src/controller/userController';
 
 const router = Router();
 
 router.patch('/updateProfile', updateProfile);
-router.post('/approveOrReject', approveOrReject);
-router.get('/:id/notification', getAllNotification);
+// router.post('/approveOrReject', approveOrReject);
+// router.get('/:id/notification', getAllNotification);
 
 export default router;
