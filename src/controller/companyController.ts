cedd5afb69d9e7ab20d2ef68a7ea44e94dd029b0
mond1alaa111 <mond1alaa111@gmail.com>
2024-06-06 13:27:32 +0800

    temp

--- src/controller/companyController.ts
+++ src/controller/companyController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 
-import { handleCreateCompany ,handleCreateBrand } from 'src/service/companyService';
+import { handleCreateCompany, handleCreateBrand } from 'src/service/companyService';
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
@@ -29,9 +29,91 @@ export const createBrand = async (req: Request, res: Response) => {
   console.log(req.body);
   try {
     const brand = await handleCreateBrand(req.body);
-    return res.status(201).json( brand );
+    return res.status(201).json(brand);
   } catch (err) {
     return res.status(400).json({ message: err });
   }
-}
+};
+export const getAllBrands = async (req: Request, res: Response) => {
+  console.log(req.body);
+  try {
+    const brands = await prisma.brand.findMany();
+    return res.status(200).json(brands);
+  } catch (err) {
+    return res.status(400).json({ message: err });
+  }
+};
 
+export const createOneCompany = async (req: Request, res: Response) => {
+  console.log(req.body);
+  try {
+    const company = await prisma.company.create({
+      data: req.body,
+    });
+    return res.status(201).json({ company });
+  } catch (err) {
+    return res.status(400).json({ message: err });
+  }
+};
+
+export const createOneBrand = async (req: Request, res: Response) => {
+  const {
+    name,
+    email,
+    phone,
+    company,
+    registration_number,
+    brandInstagram,
+    brandTiktok,
+    brandFacebook,
+    brandIntersts,
+    brandIndustries,
+  }: {
+    name: string;
+    email: string;
+    phone: string;
+    registration_number: string;
+    brandInstagram: string;
+    brandTiktok: string;
+    brandFacebook: string;
+    company: string;
+    brandIntersts: string[];
+    brandIndustries: string[];
+  } = req.body;
+  console.log(req.body);
+  try {
+    const companyInfo = await prisma.company.findFirst({
+      where: {
+        name: company,
+      },
+    });
+    const brand = await prisma.brand.create({
+      data: {
+        name: name,
+        email: email,
+        phone: phone,
+        registration_number: registration_number,
+        companyId: companyInfo?.id as string,
+        instagram: brandInstagram,
+        facebook: brandFacebook,
+        tiktok: brandTiktok,
+        intersets: brandIntersts,
+        indystries: brandIndustries,
+      },
+    });
+
+    return res.status(201).json({ brand });
+  } catch (err) {
+    return res.status(400).json({ message: err });
+  }
+
+  console.log(name);
+  res.status(200).json({ name });
+  // const brand = await prisma.brand.create({
+  //   data: {
+  //     name:name,
+
+  //   }
+  // });
+  // return res.status(201).json({ brand });
+};
--- src/routes/companyRoute.ts
+++ src/routes/companyRoute.ts
@@ -2,13 +2,16 @@ import { Router } from "express";
 
 import { isSuperAdmin } from "src/middleware/onlySuperadmin";
 
-import { createCompany , getAllCompanies ,createBrand } from "src/controller/companyController";
+import { createCompany , getAllCompanies ,createBrand ,getAllBrands ,createOneCompany ,createOneBrand } from "src/controller/companyController";
 
 const router = Router();
 
 router.post("/createCompany", isSuperAdmin, createCompany);
 router.get("/getCompanies", isSuperAdmin, getAllCompanies);
 router.post("/createBrand", isSuperAdmin, createBrand);
+router.get("/getBrands", isSuperAdmin, getAllBrands);
+router.post("/createOneCompany", isSuperAdmin, createOneCompany);
+router.post("/createOneBrand", isSuperAdmin, createOneBrand)
 
 
 export default router;
