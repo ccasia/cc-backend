98283c41141b3e09cbac60d9c84142d202915936
afiqnexea <afiqnrzm@hotmail.com>
2024-06-07 22:36:02 +0800

    a

--- src/routes/companyRoute.ts
+++ src/routes/companyRoute.ts
@@ -2,12 +2,20 @@ import { Router } from 'express';
 
 import { isSuperAdmin } from 'src/middleware/onlySuperadmin';
 
-import { createCompany, getAllCompanies, createBrand } from 'src/controller/companyController';
+import {
+  createCompany,
+  getAllCompanies,
+  createBrand,
+  getCompanyById,
+  deleteCompany,
+} from 'src/controller/companyController';
 
 const router = Router();
 
-router.post('/createCompany', isSuperAdmin, createCompany);
 router.get('/getCompanies', isSuperAdmin, getAllCompanies);
+router.get('/getCompany/:id', isSuperAdmin, getCompanyById);
+router.post('/createCompany', isSuperAdmin, createCompany);
 router.post('/createBrand', isSuperAdmin, createBrand);
+router.delete('/deleteCompany/:id', isSuperAdmin, deleteCompany);
 
 export default router;
