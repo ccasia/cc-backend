baf937d05a680de231a610f9398b1a5014f4bc87
afiqnexea <afiqnrzm@hotmail.com>
2024-07-18 21:39:01 +0800

    a

--- src/server.ts
+++ src/server.ts
@@ -15,6 +15,7 @@ import 'src/config/cronjob';
 import http from 'http';
 import { isLoggedIn } from './middleware/onlyLogin';
 import { Server, Socket } from 'socket.io';
+
 // import { getNotificationByUserId } from './controller/notificationController';
 
 dotenv.config();
@@ -100,39 +101,39 @@ app.use(passport.initialize());
 
 app.use(passport.session());
 
-passport.use(
-  new FacebookStrategy.Strategy(
-    {
-      clientID: process.env.APP_ID,
-      clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: 'https://app.cultcreativeasia.com/api/auth/facebook/callback',
-      enableProof: true,
+// passport.use(
+//   new FacebookStrategy.Strategy(
+//     {
+//       clientID: process.env.APP_ID,
+//       clientSecret: process.env.CLIENT_SECRET,
+//       callbackURL: 'https://app.cultcreativeasia.com/api/auth/facebook/callback',
+//       enableProof: true,
 
-      profileFields: ['id', 'displayName', 'photos', 'email'], // Optional fields to request
-    } as any,
-    function (accessToken: any, refreshToken: any, profile: any, done: any) {
-      // Save the accessToken and profile information in your database
-      // For now, we will just log it
-      console.log('Access Token:', accessToken);
-      console.log('Profile:', profile);
-      return done(null, profile);
-    },
-  ),
-);
+//       profileFields: ['id', 'displayName', 'photos', 'email'], // Optional fields to request
+//     } as any,
+//     function (accessToken: any, refreshToken: any, profile: any, done: any) {
+//       // Save the accessToken and profile information in your database
+//       // For now, we will just log it
+//       console.log('Access Token:', accessToken);
+//       console.log('Profile:', profile);
+//       return done(null, profile);
+//     },
+//   ),
+// );
 
 app.use(router);
 
-app.get(
-  '/auth/facebook',
-  passport.authenticate('facebook', {
-    scope: ['pages_show_list', 'business_management', 'instagram_basic', 'pages_manage_metadata'],
-  }),
-);
+// app.get(
+//   '/auth/facebook',
+//   passport.authenticate('facebook', {
+//     scope: ['pages_show_list', 'business_management', 'instagram_basic', 'pages_manage_metadata'],
+//   }),
+// );
 
-app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/' }), (req, res) => {
-  // Successful authentication
-  res.redirect('/');
-});
+// app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/' }), (req, res) => {
+//   // Successful authentication
+//   res.redirect('/');
+// });
 
 app.get('/', (_req: Request, res: Response) => {
   res.send('Server is running...');
@@ -147,6 +148,26 @@ app.get('/users', isLoggedIn, async (_req, res) => {
     console.log(error);
   }
 });
+
+// app.get('/outh', (req: Request, res: Response) => {
+//   const csrfState = Math.random().toString(36).substring(2);
+//   res.cookie('csrfState', csrfState, { maxAge: 60000 });
+
+//   let url = 'https://www.tiktok.com/v2/auth/authorize/';
+
+//   // the following params need to be in `application/x-www-form-urlencoded` format.
+//   url += `?client_key=${process.env.TIKTOK_CLIENT_KEY}`;
+//   url += '&scope=user.info.basic,user.info.profile,user.info.stats';
+//   url += '&response_type=code';
+//   url += '&redirect_uri=https://app.cultcreativeasia.com/dashboard/user/profile';
+//   url += '&state=' + csrfState;
+
+//   res.redirect(url);
+// });
+
+// app.post('/tiktok/data', (req: Request, res: Response) => {
+//   const url = 'https://open.tiktokapis.com/v2/oauth/token/';
+// });
 
 export const clients = new Map();
 
