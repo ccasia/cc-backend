226efee3e0459efa4e7229500da55a299716f52c
Fathul Amin <116142085+famintech@users.noreply.github.com>
2024-08-16 08:36:41 +0800

    added permission

--- src/routes/campaignRoute.ts
+++ src/routes/campaignRoute.ts
@@ -40,26 +40,26 @@ import { needPermissions } from 'src/middleware/needPermissions';
 const router = Router();
 
 router.get('/getAllCampaignsByAdminID', needPermissions(['list:campaign']), isSuperAdmin, getAllCampaigns);
-router.get('/getCampaignById/:id', needPermissions(['read:campaign']), isSuperAdmin, getCampaignById);
-router.get('/getAllActiveCampaign', getAllActiveCampaign);
-router.get('/matchCampaignWithCreator', matchCampaignWithCreator);
-router.get('/pitch/:id', getPitchById);
-router.get('/firstDraft', getFirstDraft);
-router.get('/timelineType', isSuperAdmin, getTimelineType);
-router.get('/defaultTimeline', isSuperAdmin, getDefaultTimeline);
-router.get('/getCampaignsBySessionId', getCampaignsByCreatorId);
-router.get('/getCampaignForCreatorById/:id', isLoggedIn, getCampaignForCreatorById);
-router.get('/getCampaignPitch', isLoggedIn, getCampaignPitchForCreator);
+router.get('/getCampaignById/:id', needPermissions(['view:campaign']), isSuperAdmin, getCampaignById);
+router.get('/getAllActiveCampaign', needPermissions(['list:campaign']), getAllActiveCampaign);
+router.get('/matchCampaignWithCreator', needPermissions(['list:campaign']), matchCampaignWithCreator);
+router.get('/pitch/:id', needPermissions(['view:campaign']), getPitchById);
+router.get('/firstDraft', needPermissions(['list:campaign']), getFirstDraft);
+router.get('/timelineType', needPermissions(['list:campaign']), isSuperAdmin, getTimelineType);
+router.get('/defaultTimeline', needPermissions(['list:campaign']), isSuperAdmin, getDefaultTimeline);
+router.get('/getCampaignsBySessionId', needPermissions(['list:campaign']), getCampaignsByCreatorId);
+router.get('/getCampaignForCreatorById/:id', needPermissions(['view:campaign']), isLoggedIn, getCampaignForCreatorById);
+router.get('/getCampaignPitch', needPermissions(['list:campaign']), isLoggedIn, getCampaignPitchForCreator);
 
-router.get('/getSubmissions', isSuperAdmin, getSubmission);
+router.get('/getSubmissions', needPermissions(['list:campaign']), isSuperAdmin, getSubmission);
 // router.get('/pitch/:campaignId', getPitchByCampaignId);
-router.get('/getCampaignLog/:id', getCampaignLog);
+router.get('/getCampaignLog/:id', needPermissions(['view:campaign']), getCampaignLog);
 
-router.post('/updateOrCreateDefaultTimeline', updateOrCreateDefaultTimeline);
-router.post('/createCampaign', isSuperAdmin, createCampaign);
+router.post('/updateOrCreateDefaultTimeline', needPermissions(['create:campaign']), updateOrCreateDefaultTimeline);
+router.post('/createCampaign', needPermissions(['create:campaign']), isSuperAdmin, createCampaign);
 // router.post('/rejectPitch', isSuperAdmin, rejectPitch);
-router.post('/createNewTimeline', isSuperAdmin, createNewTimeline);
-router.post('/createSingleTimelineType', isSuperAdmin, createSingleTimelineType);
+router.post('/createNewTimeline', needPermissions(['create:campaign']), isSuperAdmin, createNewTimeline);
+router.post('/createSingleTimelineType', needPermissions(['create:campaign']), isSuperAdmin, createSingleTimelineType);
 router.post(
   '/uploadVideo',
   // (req, res, next) => {
@@ -68,19 +68,20 @@ router.post(
   //   });
   //   next();
   // },
+  needPermissions(['create:campaign']),
   uploadVideoTest,
 );
 
-router.patch('/pitch', creatorMakePitch);
-router.patch('/changeCampaignStage/:campaignId', changeCampaignStage);
-router.patch('/closeCampaign/:id', isSuperAdmin, closeCampaign);
-router.patch('/editCampaignInfo', isSuperAdmin, editCampaignInfo);
-router.patch('/editCampaignBrandOrCompany', isSuperAdmin, editCampaignBrandOrCompany);
-router.patch('/editCampaignDosAndDonts', isSuperAdmin, editCampaignDosAndDonts);
-router.patch('/editCampaignRequirements', isSuperAdmin, editCampaignRequirements);
-router.patch('/editCampaignTimeline/:id', isSuperAdmin, editCampaignTimeline);
-router.patch('/changePitchStatus', changePitchStatus);
+router.patch('/pitch', needPermissions(['update:campaign']), creatorMakePitch);
+router.patch('/changeCampaignStage/:campaignId', needPermissions(['update:campaign']), changeCampaignStage);
+router.patch('/closeCampaign/:id', needPermissions(['update:campaign']), isSuperAdmin, closeCampaign);
+router.patch('/editCampaignInfo', needPermissions(['update:campaign']), isSuperAdmin, editCampaignInfo);
+router.patch('/editCampaignBrandOrCompany', needPermissions(['update:campaign']), isSuperAdmin, editCampaignBrandOrCompany);
+router.patch('/editCampaignDosAndDonts', needPermissions(['update:campaign']), isSuperAdmin, editCampaignDosAndDonts);
+router.patch('/editCampaignRequirements', needPermissions(['update:campaign']), isSuperAdmin, editCampaignRequirements);
+router.patch('/editCampaignTimeline/:id', needPermissions(['update:campaign']), isSuperAdmin, editCampaignTimeline);
+router.patch('/changePitchStatus', needPermissions(['update:campaign']), changePitchStatus);
 
-router.delete('/timelineType/:id', isSuperAdmin, deleteTimelineType);
+router.delete('/timelineType/:id', needPermissions(['delete:campaign']), isSuperAdmin, deleteTimelineType);
 
 export default router;
--- src/routes/creatorRoute.ts
+++ src/routes/creatorRoute.ts
@@ -10,14 +10,15 @@ import {
   getCreatorFullInfoById,
 } from '../controller/creatorController';
 import { isSuperAdmin } from 'src/middleware/onlySuperadmin';
+import { needPermissions } from 'src/middleware/needPermissions';
 const router = Router();
 
-router.get('/getAll', isSuperAdmin, getCreators);
-router.get('/getMediaKit', isSuperAdmin, getMediaKit);
-router.get('/getCreatorByID/:id', isSuperAdmin, getCreatorByID);
-router.delete('/delete/:id', isSuperAdmin, deleteCreator);
-router.patch('/update-creator', updateCreator);
-router.patch('/update-media-kit', updateMediaKit);
-router.get('/getCreatorFullInfoById/:id', isSuperAdmin, getCreatorFullInfoById);
+router.get('/getAll', needPermissions(['list:creator']), isSuperAdmin, getCreators);
+router.get('/getMediaKit', needPermissions(['list:creator']), isSuperAdmin, getMediaKit);
+router.get('/getCreatorByID/:id', needPermissions(['view:creator']), isSuperAdmin, getCreatorByID);
+router.delete('/delete/:id', needPermissions(['delete:creator']), isSuperAdmin, deleteCreator);
+router.patch('/update-creator', needPermissions(['update:creator']), updateCreator);
+router.patch('/update-media-kit', needPermissions(['update:creator']), updateMediaKit);
+router.get('/getCreatorFullInfoById/:id', needPermissions(['view:creator']), isSuperAdmin, getCreatorFullInfoById);
 
 export default router;
