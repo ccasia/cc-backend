24754102f522a6948fac4f207c42ff15b0aac98b
afiqnexea <afiqnrzm@hotmail.com>
2024-10-08 10:56:45 +0800

    a

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,6 @@ import connectPgSimple from 'connect-pg-simple';
 import fileUpload from 'express-fileupload';
 import { PrismaClient } from '@prisma/client';
 import passport from 'passport';
-// import FacebookStrategy from 'passport-facebook';
 import '@configs/cronjob';
 import http from 'http';
 import { markMessagesAsSeen } from '@controllers/threadController';
@@ -18,7 +17,6 @@ import { isLoggedIn } from '@middlewares/onlyLogin';
 import { Server, Socket } from 'socket.io';
 import '@services/uploadVideo';
 import './helper/videoDraft';
-// import dotenvx from '@dotenvx/dotenvx';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -77,12 +75,7 @@ declare module 'express-session' {
 const pgSession = connectPgSimple(session);
 
 const pgPool = new pg.Pool({
-  // user: 'afiqdanial',
   connectionString: process.env.DATABASE_URL,
-  // host: 'localhost',
-  // database: 'cult_creative',
-  // password: 'postgres',
-  // port: 5431,
 });
 
 const sessionMiddleware = session({
