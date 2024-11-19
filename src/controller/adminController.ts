00ea2e93fb1a967924868b8d50a1c009f672a512
Afiq Danial <afiq@nexea.co>
2024-11-19 11:06:52 +0800

    a

--- src/controller/adminController.ts
+++ src/controller/adminController.ts
@@ -5,7 +5,7 @@ export const deleteAdminById = async (req: Request, res: Response) => {
   const { id } = req.params;
   try {
     await handleDeleteAdminById(id);
-    return res.sendStatus(200);
+    return res.status(200).json({ message: 'Admin deleted.' });
   } catch (err) {
     return res.status(400).json({ message: err });
   }
--- src/service/adminService.ts
+++ src/service/adminService.ts
@@ -4,28 +4,73 @@ const prisma = new PrismaClient();
 
 export const handleDeleteAdminById = async (id: string) => {
   try {
-    const res = await prisma.$transaction([
-      prisma.adminPermissionModule.deleteMany({
-        where: {
-          admin: {
-            userId: id,
-          },
-        },
-      }),
-      prisma.admin.delete({
-        where: {
-          userId: id,
-        },
-      }),
-      prisma.user.delete({
-        where: {
-          id,
-        },
-      }),
+    const admin = await prisma.user.findUnique({
+      where: { id: id },
+      include: { admin: true },
+    });
+
+    if (!admin) throw new Error('Admin not found.');
+
+    await prisma.$transaction([
+      prisma.message.deleteMany({ where: { senderId: id } }),
+      prisma.userThread.deleteMany({ where: { userId: id } }),
+      prisma.unreadMessage.deleteMany({ where: { userId: id } }),
+      prisma.seenMessage.deleteMany({ where: { userId: id } }),
+      prisma.bookMarkCampaign.deleteMany({ where: { userId: id } }),
+      prisma.campaignLog.deleteMany({ where: { adminId: id } }),
+      prisma.campaignTaskAdmin.deleteMany({ where: { userId: id } }),
+      prisma.feedback.deleteMany({ where: { adminId: id } }),
+      prisma.notification.deleteMany({ where: { userId: id } }),
+      prisma.userNotification.deleteMany({ where: { userId: id } }),
+      prisma.resetPasswordToken.deleteMany({ where: { userId: id } }),
+      prisma.invoice.deleteMany({ where: { adminId: id } }),
+      prisma.campaignAdmin.deleteMany({ where: { adminId: admin.admin?.id } }),
     ]);
-    return res;
+
+    await prisma.admin.delete({
+      where: {
+        userId: id,
+      },
+    });
+
+    await prisma.user.delete({
+      where: { id: id },
+    });
+
+    // const res = await prisma.$transaction([
+    //   prisma.adminPermissionModule.deleteMany({
+    //     where: {
+    //       admin: {
+    //         userId: id,
+    //       },
+    //     },
+    //   }),
+    //   prisma.campaignAdmin.deleteMany({
+    //     where: {
+    //       admin: {
+    //         userId: id,
+    //       },
+    //     },
+    //   }),
+    //   prisma.admin.delete({
+    //     where: {
+    //       userId: id,
+    //     },
+    //   }),
+    //   prisma.user.delete({
+    //     where: {
+    //       id,
+    //     },
+    //   }),
+    //   prisma.unreadMessage.deleteMany({
+    //     where: {
+    //       userId: id,
+    //     },
+    //   }),
+    // ]);
+    // return res;
   } catch (error) {
-    //console.log(error);
+    console.log(error);
     return error;
   }
 };
