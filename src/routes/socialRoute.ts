e31a4faf2999d947a16e698a28f443286dad5e0b
Afiq Danial <afiq@nexea.co>
2025-01-06 22:54:17 +0800

    a

--- src/routes/socialRoute.ts
+++ src/routes/socialRoute.ts
@@ -106,7 +106,7 @@ router.get('/tiktok', async (req: Request, res: Response) => {
   }
 });
 
-router.get('/auth/facebook/callback', (req: Request, res: Response) => {
+router.get('/auth/facebook', (req: Request, res: Response) => {
   const scopes =
     'pages_show_list,business_management,instagram_basic,instagram_manage_comments,instagram_manage_insights,pages_read_engagement';
   const facebookLoginUrl = `https://www.facebook.com/v21.0/dialog/oauth?client_id=${FACEBOOK_APP_ID}&redirect_uri=${REDIRECT_URI}&scope=${scopes}`;
