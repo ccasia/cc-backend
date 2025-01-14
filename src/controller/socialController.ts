ea81d6830db95e5593d53d6f39f9ade99f358f9c
Afiq Danial <afiq@nexea.co>
2025-01-14 23:27:56 +0800

    a

--- src/controller/socialController.ts
+++ src/controller/socialController.ts
@@ -1,24 +1,118 @@
+import { decryptToken, encryptToken } from '@helper/encrypt';
+import axios from 'axios';
 import { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client';
 
-const TIKTOK_REDIRECT_URI = `https://staging.cultcreativeasia.com/api/social/tiktok/callback`;
-const CLIENT_KEY = 'sbawx99tuchkscwygv';
+// const CODE_VERIFIER = 'your_unique_code_verifier';
+// const CODE_CHALLENGE = 'SHA256_hash_of_code_verifier';
 
-const CODE_VERIFIER = 'your_unique_code_verifier';
-const CODE_CHALLENGE = 'SHA256_hash_of_code_verifier';
+const prisma = new PrismaClient();
 
-export const redirectTiktok = (req: Request, res: Response) => {
+// Connect account
+export const tiktokAuthentication = (req: Request, res: Response) => {
   const csrfState = Math.random().toString(36).substring(2);
   res.cookie('csrfState', csrfState, { maxAge: 60000 });
 
   let url = 'https://www.tiktok.com/v2/auth/authorize/';
 
-  url += '?client_key=' + CLIENT_KEY;
+  url += '?client_key=' + process.env.TIKTOK_CLIENT_KEY;
   url += '&scope=user.info.basic,user.info.profile,user.info.stats,video.list';
   url += '&response_type=code';
-  url += '&redirect_uri=' + TIKTOK_REDIRECT_URI;
+  url += '&redirect_uri=' + process.env.TIKTOK_REDIRECT_URI;
   url += '&state=' + csrfState;
-  url += '&code_challenge=' + CODE_VERIFIER;
+  // url += '&code_challenge=' + CODE_VERIFIER;
   url += '&code_challenge_method=S256';
 
   res.send(url);
 };
+
+// Get refresh token and access token
+export const redirectTiktokAfterAuth = async (req: Request, res: Response) => {
+  const code = req.query.code;
+
+  try {
+    // Exchange code for access token
+    const tokenResponse = await axios.post(
+      'https://open.tiktokapis.com/v2/oauth/token/',
+      {
+        client_key: process.env.TIKTOK_CLIENT_KEY,
+        client_secret: process.env.TIKTOK_CLIENT_SECRET,
+        code,
+        grant_type: 'authorization_code',
+        redirect_uri: process.env.TIKTOK_REDIRECT_URI,
+      },
+      {
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      },
+    );
+
+    const { access_token, refresh_token } = tokenResponse.data;
+
+    const encryptedAccessToken = encryptToken(access_token);
+    const encryptedRefreshToken = encryptToken(refresh_token);
+
+    await prisma.creator.update({
+      where: {
+        userId: req.session.userid,
+      },
+      data: {
+        tiktokData: { ...tokenResponse.data, access_token: encryptedAccessToken, refresh_token: encryptedRefreshToken },
+      },
+    });
+
+    res.redirect('https://staging.cultcreativeasia.com/dashboard/user/profile');
+  } catch (error) {
+    console.error('Error during TikTok OAuth:', error.response?.data || error.message);
+    res.status(500).send('Error during TikTok OAuth');
+  }
+};
+
+// Get Tiktok Data
+export const tiktokData = async (req: Request, res: Response) => {
+  const { userId } = req.params;
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+      select: {
+        creator: true,
+      },
+    });
+
+    if (!user) return res.status(404).json({ message: 'User not found.' });
+
+    let accessToken = (user.creator?.tiktokData as any)?.access_token;
+
+    accessToken = decryptToken(accessToken);
+
+    // Get user profile info
+    const userInfoResponse = await axios.get('https://open.tiktokapis.com/v2/user/info/', {
+      params: {
+        fields: 'open_id, union_id, display_name, avatar_url, following_count, follower_count, likes_count',
+      },
+      headers: { Authorization: `Bearer ${accessToken}` },
+    });
+
+    // Get user video lists
+    const videoInfoResponse = await axios.post(
+      'https://open.tiktokapis.com/v2/video/list/',
+      { max_count: 20 },
+      {
+        params: {
+          fields: 'cover_image_url, id, title, video_description, duration, embed_link',
+        },
+        headers: { Authorization: `Bearer ${accessToken}`, 'Content-Type': 'application/json' },
+      },
+    );
+
+    const data = { user: userInfoResponse.data, videos: videoInfoResponse.data };
+
+    return res.status(200).json(data);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json(error);
+  }
+};
+
+// Revoke access
--- src/routes/socialRoute.ts
+++ src/routes/socialRoute.ts
@@ -1,110 +1,21 @@
-import { redirectTiktok } from '@controllers/socialController';
-import { encryptToken, decryptToken } from '@helper/encrypt';
+import { redirectTiktokAfterAuth, tiktokAuthentication, tiktokData } from '@controllers/socialController';
+import { Router, Request, Response } from 'express';
 import { isLoggedIn } from '@middlewares/onlyLogin';
 import { PrismaClient } from '@prisma/client';
 import axios from 'axios';
-import { Router, Request, Response } from 'express';
 
 const router = Router();
-
-const CLIENT_KEY = 'sbawx99tuchkscwygv';
-const CLIENT_SECRET = 'oIjGT7T8WJPWwL2POjTyYI75WRwVj8nh';
-const TIKTOK_REDIRECT_URI = `https://staging.cultcreativeasia.com/api/social/tiktok/callback`;
 
 const FACEBOOK_APP_ID = '1912785502516586';
 const REDIRECT_URI = 'https://staging.cultcreativeasia.com/api/social/auth/facebook/callback';
 
 const prisma = new PrismaClient();
 
-router.get('/oauth/tiktok', isLoggedIn, redirectTiktok);
+router.get('/oauth/tiktok', isLoggedIn, tiktokAuthentication);
 
-router.get('/tiktok/callback', async (req: Request, res: Response) => {
-  const code = req.query.code;
+router.get('/tiktok/callback', isLoggedIn, redirectTiktokAfterAuth);
 
-  try {
-    // Exchange code for access token
-    const tokenResponse = await axios.post(
-      'https://open.tiktokapis.com/v2/oauth/token/',
-      {
-        client_key: CLIENT_KEY,
-        client_secret: CLIENT_SECRET,
-        code,
-        grant_type: 'authorization_code',
-        redirect_uri: TIKTOK_REDIRECT_URI,
-      },
-      {
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      },
-    );
-
-    const { access_token, refresh_token } = tokenResponse.data;
-
-    const encryptedAccessToken = encryptToken(access_token);
-    const encryptedRefreshToken = encryptToken(refresh_token);
-
-    await prisma.creator.update({
-      where: {
-        userId: req.session.userid,
-      },
-      data: {
-        tiktokToken: { encryptedAccessToken, encryptedRefreshToken },
-      },
-    });
-
-    res.redirect('https://staging.cultcreativeasia.com/dashboard/user/profile');
-  } catch (error) {
-    console.error('Error during TikTok OAuth:', error.response?.data || error.message);
-    res.status(500).send('Error during TikTok OAuth');
-  }
-});
-
-router.get('/tiktok', async (req: Request, res: Response) => {
-  const userId = req.session.userid;
-  try {
-    const user = await prisma.creator.findFirst({
-      where: {
-        userId: userId,
-      },
-    });
-
-    if (!user) return res.status(404).json({ message: 'Creator not found.' });
-
-    const tiktokToken = (user?.tiktokToken as any)?.encryptedAccessToken;
-
-    console.log('TIKTOKTOKEN', tiktokToken);
-
-    const access_token = decryptToken(tiktokToken);
-
-    console.log('ACCESSTOKEN', access_token);
-
-    //  Get user info
-    const userInfoResponse = await axios.get('https://open.tiktokapis.com/v2/user/info/', {
-      params: {
-        fields: 'open_id, union_id, display_name, avatar_url, following_count, follower_count, likes_count',
-      },
-      headers: { Authorization: `Bearer ${access_token}` },
-    });
-
-    const videoInfoResponse = await axios.post(
-      'https://open.tiktokapis.com/v2/video/list/',
-      { max_count: 20 },
-      {
-        params: {
-          fields: 'cover_image_url, id, title',
-        },
-        headers: { Authorization: `Bearer ${access_token}`, 'Content-Type': 'application/json' },
-      },
-    );
-
-    const userInfo = userInfoResponse.data;
-
-    const videoInfo = videoInfoResponse.data;
-
-    return res.json({ userInfo, videoInfo });
-  } catch (error) {
-    return res.status(400).json(error);
-  }
-});
+router.get('/tiktok/:userId', isLoggedIn, tiktokData);
 
 router.get('/auth/facebook', (req: Request, res: Response) => {
   const scopes = 'email,public_profile,pages_show_list,business_management,instagram_basic';
