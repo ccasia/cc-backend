a94e38c83cb566b0762f65de3a873ffe4d99508c
Afiq Danial <afiq@nexea.co>
2025-01-17 19:45:52 +0800

    a

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,7 @@ import { handleSendMessage, fetchMessagesFromThread } from '@services/threadServ
 import { isLoggedIn } from '@middlewares/onlyLogin';
 import { Server } from 'socket.io';
 import '@services/uploadVideo';
-import './helper/videoProcess';
+import './helper/test';
 import '@helper/processPitchVideo';
 import './helper/videoDraft';
 // import './helper/videoDraftWorker';
