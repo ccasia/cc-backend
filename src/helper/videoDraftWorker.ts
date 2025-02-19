d05db1c964516b156180ad931b80dd82b1e8564e
Afiq Danial <afiq@nexea.co>
2025-02-19 13:33:01 +0800

    a

--- src/helper/videoDraftWorker.ts
+++ src/helper/videoDraftWorker.ts
@@ -11,7 +11,6 @@ import { activeProcesses, clients, io } from '../server';
 import { Entity, PrismaClient } from '@prisma/client';
 import { saveNotification } from '@controllers/notificationController';
 import { spawn } from 'child_process';
-
 import dayjs from 'dayjs';
 import { notificationDraft } from './notification';
 import { createNewTask, getTaskId, updateTask } from '@services/kanbanService';
@@ -23,203 +22,169 @@ Ffmpeg.setFfprobePath(ffprobePath.path);
 const prisma = new PrismaClient();
 const pool = workerpool.pool();
 
-const processVideo = async (
-  videoData: any,
-  inputPath: string,
-  outputPath: string,
-  submissionId: string,
-  fileName: string,
-  folder: string,
-  caption: string,
-) => {
+interface VideoData {
+  userid: string;
+  inputPath: string;
+  outputPath: string;
+  submissionId: string;
+  fileName: string;
+  folder: string;
+  caption: string;
+  admins: { admin: { user: { id: string } } }[];
+}
+
+const processVideo = async (videoData: VideoData): Promise<void> => {
   return new Promise<void>((resolve, reject) => {
-    const userid = videoData.userid;
+    const { userid, inputPath, outputPath, submissionId, fileName, folder, caption } = videoData;
     const command = Ffmpeg(inputPath)
       .outputOptions([
         '-c:v libx264',
         '-crf 26',
         '-pix_fmt yuv420p',
         '-preset ultrafast',
-        '-map 0:v:0', // Select the first video stream
+        '-map 0:v:0',
         '-map 0:a:0?',
         '-threads 4',
       ])
       .save(outputPath)
-      .on('progress', (progress: any) => {
+      .on('progress', (progress) => {
         activeProcesses.set(submissionId, command);
-        const percentage = Math.round(progress.percent);
-        if (io) {
-          io.to(clients.get(userid)).emit('progress', {
-            progress: percentage,
-            submissionId: submissionId,
-            name: 'Compression Start',
-          });
-        }
+        const percentage = Math.round(progress.percent || 0);
+        io?.to(clients.get(userid)!).emit('progress', {
+          progress: percentage,
+          submissionId,
+          name: 'Compression Start',
+        });
       })
       .on('end', async () => {
-        const size = await new Promise((resolve, reject) => {
-          fs.stat(outputPath, (err, data) => {
-            if (err) {
-              reject();
-            }
-            resolve(data.size);
-          });
-        });
-
-        const publicURL = await uploadPitchVideo(
-          outputPath,
-          fileName,
-          folder,
-          (data: number) => {
-            if (io) {
-              io.to(clients.get(userid)).emit('progress', {
+        try {
+          const size = (await fs.promises.stat(outputPath)).size;
+          const publicURL = await uploadPitchVideo(
+            outputPath,
+            fileName,
+            folder,
+            (data: number) => {
+              io?.to(clients.get(userid)!).emit('progress', {
                 progress: data,
-                submissionId: submissionId,
+                submissionId,
                 name: 'Uploading Start',
               });
-            }
-          },
-          size as number,
-        );
+            },
+            size,
+          );
 
-        const data = await prisma.submission.update({
-          where: {
-            id: submissionId,
-          },
-          data: {
-            content: publicURL,
-            caption: caption,
-            status: 'PENDING_REVIEW',
-            submissionDate: dayjs().format(),
-          },
-          include: {
-            submissionType: true,
-            campaign: {
-              include: {
-                campaignAdmin: {
-                  select: {
-                    adminId: true,
-                    admin: {
-                      select: {
-                        user: {
-                          select: {
-                            Board: {
-                              include: {
-                                columns: true,
-                              },
-                            },
-                            id: true,
-                          },
-                        },
-                      },
+          const data = await prisma.submission.update({
+            where: { id: submissionId },
+            data: {
+              content: publicURL,
+              caption,
+              status: 'PENDING_REVIEW',
+              submissionDate: dayjs().toISOString(),
+            },
+            include: {
+              submissionType: true,
+              campaign: {
+                include: {
+                  campaignAdmin: {
+                    select: {
+                      adminId: true,
+                      admin: { include: { user: { include: { Board: { include: { columns: true } } } } } },
                     },
                   },
                 },
               },
+              user: { include: { creator: true } },
             },
-            user: {
-              include: {
-                creator: true,
+          });
+
+          if (data.campaign.spreadSheetURL) {
+            const spreadSheetId = data.campaign.spreadSheetURL.split('/d/')[1].split('/')[0];
+            await createNewRowData({
+              creatorInfo: {
+                name: data.user.name as string,
+                username: data.user.creator?.instagram as string,
+                postingDate: dayjs().format('LL'),
+                caption,
+                videoLink: `https://storage.googleapis.com/${process.env.BUCKET_NAME}/${data?.submissionType.type}/${data?.id}_draft.mp4?v=${dayjs().toISOString()}`,
               },
-            },
-          },
-        });
+              spreadSheetId,
+            });
+          }
 
-        if (data.campaign.spreadSheetURL) {
-          const spreadSheetId = data.campaign.spreadSheetURL.split('/d/')[1].split('/')[0];
+          const { title, message } = notificationDraft(data.campaign.name, 'Creator');
 
-          await createNewRowData({
-            creatorInfo: {
-              name: data.user.name,
-              username: data.user.creator?.instagram,
-              postingDate: dayjs().format('LL'),
-              caption: caption,
-              videoLink: `https://storage.googleapis.com/${process.env.BUCKET_NAME as string}/${data?.submissionType.type}/${`${data?.id}_draft.mp4`}?v=${dayjs().format()}`,
-            } as any,
-            spreadSheetId: spreadSheetId,
-          });
-        }
-
-        const { title, message } = notificationDraft(data.campaign.name, 'Creator');
-
-        const notification = await saveNotification({
-          userId: data.userId,
-          message: message,
-          title: title,
-          entity: 'Draft',
-          entityId: data.campaign.id,
-        });
-
-        if (io) {
-          io.to(clients.get(data.userId)).emit('notification', notification);
-        }
-
-        const { title: adminTitle, message: adminMessage } = notificationDraft(
-          data.campaign.name,
-          'Admin',
-          data.user.name as string,
-        );
-
-        for (const item of data.campaign.campaignAdmin) {
           const notification = await saveNotification({
-            userId: item.adminId,
-            message: adminMessage,
-            creatorId: userid,
-            title: adminTitle,
+            userId: data.userId,
+            message: message,
+            title: title,
             entity: 'Draft',
-            entityId: data.campaignId,
+            entityId: data.campaign.id,
           });
 
-          if (item.admin.user.Board) {
-            const actionNeededColumn = item.admin.user.Board.columns.find((item) => item.name === 'Actions Needed');
+          io?.to(clients.get(data.userId)).emit('notification', notification);
 
-            const taskInDone = await getTaskId({
-              boardId: item.admin.user.Board.id,
-              submissionId: data.id,
-              columnName: 'Done',
+          const { title: adminTitle, message: adminMessage } = notificationDraft(
+            data.campaign.name,
+            'Admin',
+            data.user.name as string,
+          );
+
+          for (const item of data.campaign.campaignAdmin) {
+            const notification = await saveNotification({
+              userId: item.adminId,
+              message: adminMessage,
+              creatorId: userid,
+              title: adminTitle,
+              entity: 'Draft',
+              entityId: data.campaignId,
             });
 
-            if (actionNeededColumn) {
-              if (taskInDone) {
-                await updateTask({
-                  taskId: taskInDone.id,
-                  toColumnId: actionNeededColumn.id,
-                  userId: item.admin.user.id,
-                });
-              } else {
-                await createNewTask({
-                  submissionId: data.id,
-                  name: 'Draft Submission',
-                  userId: item.admin.user.id,
-                  position: 1,
-                  columnId: actionNeededColumn.id,
-                });
+            if (item.admin.user.Board) {
+              const actionNeededColumn = item.admin.user.Board.columns.find((item) => item.name === 'Actions Needed');
+
+              const taskInDone = await getTaskId({
+                boardId: item.admin.user.Board.id,
+                submissionId: data.id,
+                columnName: 'Done',
+              });
+
+              if (actionNeededColumn) {
+                if (taskInDone) {
+                  await updateTask({
+                    taskId: taskInDone.id,
+                    toColumnId: actionNeededColumn.id,
+                    userId: item.admin.user.id,
+                  });
+                } else {
+                  await createNewTask({
+                    submissionId: data.id,
+                    name: 'Draft Submission',
+                    userId: item.admin.user.id,
+                    position: 1,
+                    columnId: actionNeededColumn.id,
+                  });
+                }
               }
+            }
+
+            if (io) {
+              io.to(clients.get(item.adminId)).emit('notification', notification);
             }
           }
 
-          if (io) {
-            io.to(clients.get(item.adminId)).emit('notification', notification);
-          }
+          activeProcesses.delete(submissionId);
+          io?.to(clients.get(userid)!).emit('progress', { submissionId, progress: 100 });
+          await fs.promises.unlink(inputPath);
+          await fs.promises.unlink(outputPath);
+          resolve();
+        } catch (error) {
+          reject(error);
         }
-        activeProcesses.delete(submissionId);
-        if (io) {
-          io.to(clients.get(userid)).emit('progress', { submissionId, progress: 100 });
-        }
-        fs.unlinkSync(inputPath);
-        fs.unlinkSync(outputPath);
-        resolve();
       })
       .on('error', (err) => {
-        console.log(err);
-        if (err.message.includes('ffmpeg was killed')) {
-          // Handle known errors
-          resolve();
-        } else {
-          console.error('Error processing video:', err);
-          activeProcesses.delete(submissionId); // Clean up the map
-          reject(err); // Reject for non-cancellation errors
-        }
+        console.error('Error processing video:', err);
+        activeProcesses.delete(submissionId);
+        reject(err);
         fs.unlinkSync(inputPath);
       });
   });
@@ -227,31 +192,23 @@ const processVideo = async (
 
 (async () => {
   try {
-    const conn = await amqplib.connect(process.env.RABBIT_MQ as string);
+    const conn = await amqplib.connect(process.env.RABBIT_MQ!);
     const channel = await conn.createChannel();
     await channel.assertQueue('draft', { durable: true });
     await channel.purgeQueue('draft');
-    // await channel.prefetch(1);
     console.log('Consumer 1 Starting...');
+    const startUsage = process.cpuUsage();
 
     await channel.consume('draft', async (msg) => {
-      if (msg !== null) {
-        const content: any = JSON.parse(msg.content.toString());
-
-        await processVideo(
-          content,
-          content.inputPath,
-          content.outputPath,
-          content.submissionId,
-          content.fileName,
-          content.folder,
-          content.caption,
-        );
-
+      if (msg) {
+        const content: VideoData = JSON.parse(msg.content.toString());
+        await processVideo(content);
         channel.ack(msg);
+        const endUsage = process.cpuUsage(startUsage);
+        console.log(`CPU Usage: ${endUsage.user} microseconds (user) / ${endUsage.system} microseconds (system)`);
 
         for (const item of content.admins) {
-          io.to(clients.get(item.admin.user.id)).emit('newSubmission');
+          io.to(clients.get(item.admin.user.id)!).emit('newSubmission');
         }
 
         const allSuperadmins = await prisma.user.findMany({
@@ -266,6 +223,6 @@ const processVideo = async (
       }
     });
   } catch (error) {
-    throw new Error(error);
+    console.error('Error in message queue consumer:', error);
   }
 })();
