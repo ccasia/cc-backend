b5d58e66e4049c997bfc34f34a6e988daa3aca16
afiq <afiq@nexea.co>
2025-03-13 02:06:22 +0800

    a

--- src/controller/socialController.ts
+++ src/controller/socialController.ts
@@ -558,7 +558,7 @@ export const getInstagramOverview = async (req: Request, res: Response) => {
     const average_like = calculateAverageLikes((user.instagramUser as any).instagramVideo);
 
     // const data = { user: { ...overview, average_like }, contents: [...medias.data] };
-    const data = Object.assign(user, average_like);
+    const data = Object.assign(user, { average_like });
 
     return res.status(200).json(data);
   } catch (error) {
