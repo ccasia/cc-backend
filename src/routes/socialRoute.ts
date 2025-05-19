16df16ad4a4c4347fe24d20324f54b947be2b02f
afiq <afiq@nexea.co>
2025-05-20 02:49:11 +0800

    a

--- src/routes/socialRoute.ts
+++ src/routes/socialRoute.ts
@@ -1,5 +1,6 @@
 import {
   facebookAuthentication,
+  getInstagramMediaInsight,
   getInstagramMediaKit,
   getInstagramOverview,
   getUserInstagramData,
@@ -44,5 +45,6 @@ router.delete('/instagram/permissions/:userId', isLoggedIn, removeInstagramPermi
 
 router.get('/v2/auth/instagram/callback', handleInstagramCallback);
 router.get('/v2/instagramOverview/:userId', getInstagramMediaKit);
+router.get('/v2/mediaInsight/:userId', getInstagramMediaInsight);
 
 export default router;
