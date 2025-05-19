20585b4a36cc4e1a2ca17f3f118dc7eced314e63
cc-esther <esther@cultcreative.asia>
2025-05-19 11:20:44 +0800

    feat: add timestamp to instagram media data

--- src/service/socialMediaService.ts
+++ src/service/socialMediaService.ts
@@ -70,7 +70,7 @@ export const getInstagramUserData = async (
 export const getInstagramMediaData = async (
   accessToken: string,
   mediaId: string,
-  fields: ('like_count' | 'media_url' | 'media_type' | 'comments_count' | 'thumbnail_url' | 'caption' | 'permalink')[],
+  fields: ('like_count' | 'media_url' | 'media_type' | 'comments_count' | 'thumbnail_url' | 'caption' | 'permalink' | 'timestamp')[],
 ) => {
   try {
     const response = await axios.get(`https://graph.facebook.com/${mediaId}`, {
