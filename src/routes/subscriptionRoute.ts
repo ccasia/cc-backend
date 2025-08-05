1feaa207635655cf4638ba29091ed23cc7444867
afiq <afiq@nexea.co>
2025-08-05 15:31:38 +0800

    a

--- src/routes/subscriptionRoute.ts
+++ src/routes/subscriptionRoute.ts
@@ -1,9 +1,10 @@
-import { getAllSubscriptions } from '@controllers/subscriptionController';
+import { getAllSubscriptions, updateSubscription } from '@controllers/subscriptionController';
+import { isLoggedIn } from '@middlewares/onlyLogin';
 import { isSuperAdmin } from '@middlewares/onlySuperadmin';
 import { Router } from 'express';
 
 const router = Router();
 
 router.get('/', isSuperAdmin, getAllSubscriptions);
-
+router.patch('/:id', isLoggedIn, isSuperAdmin, updateSubscription);
 export default router;
