dea46847414d14dd3d8dae24603ffc53f1402727
afiq <afiq@nexea.co>
2025-09-03 01:46:35 +0800

    a

--- src/controller/campaignController.ts
+++ src/controller/campaignController.ts
@@ -7010,7 +7010,7 @@ export const shortlistGuestCreators = async (req: Request, res: Response) => {
 };
 
 export const getCampaignsForPublic = async (req: Request, res: Response) => {
-  const { cursor, take = 10, search } = req.query;
+  const { cursor, take = 10, search, campaignId } = req.query;
   try {
     let campaigns = await prisma.campaign.findMany({
       take: Number(take),
@@ -7021,14 +7021,20 @@ export const getCampaignsForPublic = async (req: Request, res: Response) => {
         },
       }),
       where: {
-        // status: 'ACTIVE',
-
         AND: [
           { status: 'ACTIVE' },
           {
             ...(search && {
               name: {
                 contains: search as string,
+                mode: 'insensitive',
+              },
+            }),
+          },
+          {
+            ...(campaignId && {
+              id: {
+                equals: campaignId as string,
                 mode: 'insensitive',
               },
             }),
