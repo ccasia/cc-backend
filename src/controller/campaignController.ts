c6eba9f4f12327837d4fadbfcd254bd3e6c6893e
afiq <afiq@nexea.co>
2025-09-03 02:20:13 +0800

    a

--- src/controller/campaignController.ts
+++ src/controller/campaignController.ts
@@ -7021,25 +7021,16 @@ export const getCampaignsForPublic = async (req: Request, res: Response) => {
         },
       }),
       where: {
-        AND: [
-          { status: 'ACTIVE' },
-
-          {
-            ...(search && {
-              name: {
-                contains: search as string,
-                mode: 'insensitive',
-              },
-            }),
+        status: 'ACTIVE',
+        ...(campaignId && {
+          id: campaignId as string,
+        }),
+        ...(search && {
+          name: {
+            contains: search as string,
+            mode: 'insensitive',
           },
-          {
-            ...(campaignId && {
-              id: {
-                equals: campaignId as string,
-              },
-            }),
-          },
-        ],
+        }),
       },
       include: {
         campaignBrief: true,
