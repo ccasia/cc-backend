f00d08892e33747dfccb2605bccdd24607635807
cc-esther <esther@cultcreative.asia>
2025-09-23 14:46:25 +0800

    test

--- src/service/submissionV4Service.ts
+++ src/service/submissionV4Service.ts
@@ -443,7 +443,7 @@ export const createContentSubmissionsAfterAgreement = async (agreementSubmission
 
     const contentSubmissions = [];
 
-    // Create VIDEO submissions based on ugcVideos count
+    // Create VIDEO submissions based on ugcVideos countt
     const ugcVideos = shortlistedCreator.ugcVideos || 0;
     for (let i = 1; i <= ugcVideos; i++) {
       contentSubmissions.push({
