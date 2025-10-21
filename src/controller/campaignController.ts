238cab54cbb571fcfb1e9a1e3ba95be658a45251
afiq <afiq@nexea.co>
2025-10-21 21:29:42 +0800

    a

--- src/controller/campaignController.ts
+++ src/controller/campaignController.ts
@@ -7532,7 +7532,7 @@ export const getCampaignsForPublic = async (req: Request, res: Response) => {
 
   try {
     const campaigns = await prisma.campaign.findMany({
-      take: Number(take),
+      take: Number(take) + 1,
       // ...(cursor && {
       //   skip: 1,
       //   cursor: {
