8cee07b784cc3a2ef1446fbb21527dd9f409788e
afiq <afiq@nexea.co>
2025-10-28 00:21:16 +0800

    a

--- src/controller/invoiceController.ts
+++ src/controller/invoiceController.ts
@@ -751,7 +751,7 @@ export const updateInvoice = async (req: Request, res: Response) => {
         const creatorUser = updatedInvoice.creator.user;
         const creatorPaymentForm = creatorUser?.paymentForm;
         const campaign = updatedInvoice.campaign;
-        const agreement = updatedInvoice.user?.creatorAgreement.find((item) => item.campaignId === campaignId);
+        const agreement = updatedInvoice.creator.user.creatorAgreement.find((item) => item.campaignId === campaignId);
 
         console.log('AGREEMENT:', agreement);
 
