cc3fc5a76b826cd79d4e1ba7177589c8db5172af
sam <shenqjye93@gmail.com>
2025-11-18 23:33:31 +0800

    feat: added product create and read for campaigns

--- src/routes/logisticsRoute.ts
+++ src/routes/logisticsRoute.ts
@@ -5,14 +5,30 @@ import { isAdminOrClient } from '@middlewares/adminOrClient';
 import { isSuperAdmin } from '@middlewares/onlySuperadmin';
 import { checkCampaignAccess } from '@middlewares/checkCampaignAccess';
 
-import { getCreatorLogistics, getLogisticsForCampaign } from '@controllers/logisticsController';
+import {
+  getCreatorLogistics,
+  getLogisticsForCampaign,
+  getCreatorLogisticForCampaign,
+  getProductsForCampaign,
+  createProduct,
+} from '@controllers/logisticsController';
 
 const router = express.Router();
 
 // creator routes
-router.get('/me', isLoggedIn, isCreator, getCreatorLogistics);
+router.get('/me', isLoggedIn, isCreator, getCreatorLogistics); // maybe no need
+router.get('/creator/campaign/:campaignId', isLoggedIn, isCreator, getCreatorLogisticForCampaign);
 
 // admin & client routes
-router.get('/campaign/:campaignId', isLoggedIn, isAdminOrClient, isSuperAdmin, checkCampaignAccess, getLogisticsForCampaign);
+router.get(
+  '/campaign/:campaignId',
+  isLoggedIn,
+  isAdminOrClient,
+  isSuperAdmin,
+  checkCampaignAccess,
+  getLogisticsForCampaign,
+);
+router.get('/products/campaign/:campaignId', isLoggedIn, isAdminOrClient, checkCampaignAccess, getProductsForCampaign);
+router.post('/products/:campaignId', isLoggedIn, isAdminOrClient, checkCampaignAccess, createProduct);
 
 export default router;
