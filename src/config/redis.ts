bdb4b79977980fbc5837f55f117680650af3de24
afiq <afiq@nexea.co>
2026-02-25 09:09:05 +0800

    a

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -4,7 +4,7 @@ const connection = new IORedis({
   port: 6379,
   host: 'redis',
   maxRetriesPerRequest: null,
-  password: process.env.REDIS_PASSWORD,
+  password: process.env.REDIS_PASSWORD || 'cult-redis',
 });
 
 export const subClient = connection.duplicate();
