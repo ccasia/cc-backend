042391965b4c876a75bfef1776c8e7487283fd71
afiq <afiq@nexea.co>
2026-04-03 14:24:12 +0800

    added logic to handle new role – sales and markering

--- src/controller/authController.ts
+++ src/controller/authController.ts
@@ -1160,6 +1160,8 @@ export const getprofile = async (req: Request, res: Response) => {
   try {
     const user = await getUser(userId);
 
+    console.log(user);
+
     if (
       user?.role === 'superadmin' ||
       (user?.role === 'admin' && user?.admin?.role?.name.toLowerCase() === 'finance')
