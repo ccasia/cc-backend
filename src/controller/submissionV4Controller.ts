674d31e9f54e8807b0feb16a2362a2dff8a58e58
afiq <afiq@nexea.co>
2026-04-06 16:32:59 +0800

    a

--- src/controller/submissionV4Controller.ts
+++ src/controller/submissionV4Controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { FeedbackStatus, SubmissionStatus } from '@prisma/client';
+import { FeedbackStatus, PrismaClient, SubmissionStatus } from '@prisma/client';
 import {
   createV4SubmissionsForCreator,
   getV4Submissions,
