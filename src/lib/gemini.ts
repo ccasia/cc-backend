dc0baee63e12d83dd2083b2e60679ce699ed501a
afiq <afiq@nexea.co>
2026-04-11 23:57:03 +0800

    a

--- src/lib/gemini.ts
+++ src/lib/gemini.ts
@@ -1,5 +1,5 @@
 import { ChatGoogleGenerativeAI } from '@langchain/google-genai';
-import { PrismaClient } from '@prisma/client';
+import { AiModel, PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export type GeminiModelName = 'gemini-1.5-flash' | 'gemini-1.5-pro' | 'gemini-2.0-flash-exp';
@@ -12,17 +12,13 @@ export const GEMINI_MODEL = (process.env.GEMINI_MODEL as GeminiModelName) || 'ge
  * @param temperature  0–1. Low = more factual/consistent (default 0.2)
  * @param maxTokens    Max output tokens (default 1500)
  */
-export async function createGemini(temperature = 0.2, maxTokens = 1500): Promise<ChatGoogleGenerativeAI> {
-  // if (!process.env.GOOGLE_API_KEY) {
-  //   throw new Error('GOOGLE_API_KEY is not set in .env');
-  // }
-
-  const aiModel = await prisma.aiModel.findFirst();
+export async function createGemini(aiModel: AiModel): Promise<ChatGoogleGenerativeAI> {
+  if (!aiModel.apiKey) throw new Error('Api key not found');
 
   return new ChatGoogleGenerativeAI({
     model: aiModel?.model || 'gemini-2.5-flash',
     apiKey: aiModel?.apiKey || process.env.GOOGLE_API_KEY,
-    temperature: aiModel?.temperature || temperature,
-    maxOutputTokens: aiModel?.maxOutputTokens || maxTokens,
+    temperature: aiModel?.temperature || 0.7,
+    maxOutputTokens: aiModel?.maxOutputTokens || 1024,
   });
 }
