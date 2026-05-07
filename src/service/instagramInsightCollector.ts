53570874fa60852b2fa8c0fea89d00164c4092bd
afiq <afiq@nexea.co>
2026-05-07 15:48:38 +0800

    a

--- src/service/instagramInsightCollector.ts
+++ src/service/instagramInsightCollector.ts
@@ -267,13 +267,18 @@ export async function fetchInstagramCampaignMetrics(campaignId: string): Promise
   const manualIgEngRate = manualIg.reduce((acc, cur) => acc + cur.engagementRate, 0);
   const manualTtEngRate = manualTt.reduce((acc, cur) => acc + cur.engagementRate, 0);
 
-  const totalCreators = ig.creatorPersonas.length + tt.creatorPersonas.length + manualIg.length + manualTt.length;
+  // const totalCreators = ig.creatorPersonas.length + tt.creatorPersonas.length + manualIg.length + manualTt.length;
 
-  console.log(totalCreators, allUrls);
+  // console.log(totalCreators, allUrls);
 
   const engagementRevised = Math.max(
-    +(igEngRate + tiktokEngRate + manualIgEngRate + manualTtEngRate / totalCreators).toFixed(2),
+    +((igEngRate + tiktokEngRate + manualIgEngRate + manualTtEngRate) / allUrls.length).toFixed(2),
   );
+
+  console.log('MAN - IG RATE', manualIgEngRate);
+  console.log('MAN - TT RATE', manualTtEngRate);
+  console.log('AUTO - IG RATE', igEngRate);
+  console.log('AUTO - TT RATE', tiktokEngRate);
 
   console.log(engagementRevised);
 
