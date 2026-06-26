da6b93972acdb2a6f1c0d9c2f11f6a1ccc62522d
Willy Wong <mcprojake76@gmail.com>
2026-06-26 16:19:35 +0800

    fix(invoice): process Xero webhook async with rate-limit backoff

--- src/utils/queue.ts
+++ src/utils/queue.ts
@@ -8,3 +8,7 @@ export const invoiceQueue = new Queue('invoice-queue', {
 export const bulkInvoiceQueue = new Queue('bulk-invoice-queue', {
   connection,
 });
+
+export const xeroWebhookQueue = new Queue('xero-webhook-queue', {
+  connection,
+});
